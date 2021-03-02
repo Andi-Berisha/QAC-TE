90a0b15cd332abe7ad085d8bdf5a78a9d07bf2ab
Celina <cbasa@qa.com>
2021-03-02 17:20:18 +0000

    Added Listings Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import Navigation from './Navigation';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './Footer';
 import Home from './Pages/HomePage';
+import Listings from './Pages/Listings';
 
 
 function App() {
   return (
     <Router>
       <Navigation />
-      <Footer/>
+      <Footer />
       <Switch>
         <Route path="/" exact>
-          <Home/>
+          <Home />
+        </Route>
+        <Route path="/whatsOn" exact>
+          <Listings />
         </Route>
       </Switch>
     </Router>
