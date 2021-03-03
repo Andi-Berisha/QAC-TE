525a97f8375be892cf4a2bce384aba4434a12ae8
Andi Berisha <aberisha@qa.com>
2021-03-03 09:44:08 +0000

    AB: StarRating component created

---
+++ frontend/src/Pages/StarRating.jsx
@@ -0,0 +1,44 @@
+
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const StarRating = () => {
+    const [rating, setRating] = useState(null);
+    const [hover, setHover] = useState(null);
+
+    return <div>
+
+        <p style={{ color: "green", padding: "50px" }} > The Rating is {rating}: </p>
+        {[...Array(5)].map((star, i) => {
+            const ratingValue = i + 1;
+
+            return <label>
+                <input
+                    type="radio"
+                    name="rating"
+                    value={ratingValue}
+                    onClick={() => setRating(ratingValue)}
+
+                />
+
+                <FaStar
+                    className="star"
+                    color={ratingValue <= (hover || rating) ? "#ffc107" : "#e4e5e9"}
+                    size={100}
+                    onMouseEnter={() => setHover(ratingValue)}
+                    onMouseLeave={() => setHover(null)}
+                />
+
+            </label>;
+        })}
+
+
+
+   
+
+
+
+    </div>
+}
+
+export default StarRating
