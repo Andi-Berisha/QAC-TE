a9518178ae761dc8deb99e05c224afca79f87bea
Andi Berisha <aberisha@qa.com>
2021-03-03 10:06:18 +0000

    AB: BackEnd started and css for starRating"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './Footer';
 import Home from './Pages/HomePage';
 import ListingsPage from './Pages/ListingsPage';
+import StarRating from './Pages/StarRating';
+import DiscussionBoard from './Pages/DiscussionBoard'
 
 
 function App() {
@@ -17,6 +19,9 @@ function App() {
         </Route>
         <Route path="/whatsOn" exact>
           <ListingsPage />
+        </Route>
+        <Route path="/discussionBoard" exact>
+          <DiscussionBoard/>
         </Route>
       </Switch>
     </Router>
