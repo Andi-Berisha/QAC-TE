08736bd3353cbaf6ebe887fb8b91dcba089c26dc
calfonsoQA <75623928+calfonsoQA@users.noreply.github.com>
2021-03-04 10:56:23 +0000

    booking form entries, showing times array tbd

--- frontend/src/Pages/BookingsAndPayment/BookingsPage.jsx
+++ frontend/src/Pages/BookingsAndPayment/BookingsPage.jsx
@@ -15,35 +15,42 @@ const BookingsPage = () => {
     const [filmObj, setFilmObj] = useState(films);
 
 
+    // setShowingTime(films.find(films.Showtimes, films.Showtimes, `${title}`));
+    // setShowingTime(films.find(x => x.title === `${title}`).Showtimes);
+    // setShowingTime(films.find(item => item.title === `${title}`));
+
+
     return (
         <div className="container">
             <Row>
-                <Col>
-                    <img src="https://i.imgur.com/VmLLlbK.jpg" />
+                <Col > 
+                <div class="media" style={{ marginLeft: '100px' }}>
+                    <img src="https://i.imgur.com/sYIlBfm.jpg" height="500px" class="align-self-center" />
+                    </div>
                 </Col>
-                <Col style={{ color: "white" }}>
+                <Col style={{ color: "white", backgroundColor: "grey" }}>
                     <Form>
 
                         <FormGroup>
                             <Label for="filmSelect">Select Film</Label>
                             <Input type="select" name="select" id="exampleSelect" onChange={({ target }) => {setTitle(target.value); 
-                            setFilmObj(target.obj)}}>
+                           setShowingTime(films.find(item => item.title === `${title}`));}}>
                                 {films.map((item) =>
                                     <option obj = {item} value={item.Title}>{item.Title}</option>)}
                             </Input>
                         </FormGroup>
-                        <FormGroup>
+                        {/* <FormGroup>
                             <Label for="showingTimes">Showing Times</Label>
                             <Input type="select" name="select" id="exampleSelect" onChange={({ target }) => {setTitle(target.value); 
-                            setFilmObj(target.obj)}}>
+                            setFilmObj(target.obj); }}>
                                 {films.map((item) =>
-                                    <option obj = {item} value={item.Showtimes.Times}>{item.Showtimes.Times}</option>)}
+                                    <option obj = {item} value={item.Showtimes}>{item.Showtimes}</option>)}
                             </Input>
 
-                        </FormGroup>
+                        </FormGroup> */}
                         <FormGroup>
                             <Label for="showingTimes">Showing Times</Label>
-                            {filmObj.map((item) =>
+                            {films.map((item) =>
                                 <Badge color="warning" pill style={{ marginLeft: '15px' }}>
                                     <Link to="" style={{ color: 'black' }}>{item.Showtimes}</Link>
                                 </Badge>)}
@@ -51,14 +58,36 @@ const BookingsPage = () => {
                         </FormGroup>
                         <FormGroup>
                             <Label for="exampleEmail">Name</Label>
-                            <Input type="text" name="name" id="bookerName" placeholder="Name of Booker" />
+                            <Input type="text" name="name" id="bookerName" placeholder="Name of Booker" onChange={({ target }) => setBookgerName(target.value)}/>
                         </FormGroup>
                         <FormGroup>
                             <Label for="seating">Seating</Label>
                             <Row>
                                 <Col>Adult:</Col>
                                 <Col>
-                                    <Input type="select" name="select" id="exampleSelect">
+                                    <Input type="select" name="select" id="adultSelect" onChange={({ target }) => setAdult(target.value)}>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                    </Input>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>Child:</Col>
+                                <Col>
+                                    <Input type="select" name="select" id="childSelect" onChange={({ target }) => setChild(target.value)}>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                    </Input>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>Concession:</Col>
+                                <Col>
+                                    <Input type="select" name="select" id="concessionSelect" onChange={({ target }) => setConcession(target.value)}>
                                         <option>1</option>
                                         <option>2</option>
                                         <option>3</option>
@@ -67,9 +96,12 @@ const BookingsPage = () => {
                                 </Col>
                             </Row>
                             {/* <Input type="text" name="name" id="bookerName" placeholder="Name of Booker" /> */}
+                            
+                            
                         </FormGroup>
+                        <Button color="warning" style={{width: 100}}>Pay Now</Button>
                     </Form>
-
+                    {showingTime}
                 </Col>
             </Row>
         </div>
