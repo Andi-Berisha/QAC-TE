a87afc6ef5f133ea945d931decb39a87a7aa0707
calfonsoQA <75623928+calfonsoQA@users.noreply.github.com>
2021-03-04 11:53:00 +0000

    showing times for individual film, wrong film

--- frontend/src/Pages/BookingsAndPayment/BookingsPage.jsx
+++ frontend/src/Pages/BookingsAndPayment/BookingsPage.jsx
@@ -6,26 +6,31 @@ const BookingsPage = () => {
 
     const [poster, setPoster] = useState('');
 
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState('Avengers: Endgame');
     const [showingTime, setShowingTime] = useState('');
     const [bookerName, setBookgerName] = useState('');
     const [adult, setAdult] = useState(0);
     const [child, setChild] = useState(0);
     const [concession, setConcession] = useState(0);
     const [filmObj, setFilmObj] = useState(films);
+    const [filmIndividual, setfilmIndividual] = useState(films.find(item => item.Title === title));
 
 
     // setShowingTime(films.find(films.Showtimes, films.Showtimes, `${title}`));
     // setShowingTime(films.find(x => x.title === `${title}`).Showtimes);
     // setShowingTime(films.find(item => item.title === `${title}`));
 
+    // setShowingTimeList(films.find(item => item.id === 1));
+
+    // setShowingTimeList(films.find(item => item.Title === title));
+
 
     return (
         <div className="container">
             <Row>
-                <Col > 
-                <div class="media" style={{ marginLeft: '100px' }}>
-                    <img src="https://i.imgur.com/sYIlBfm.jpg" height="500px" class="align-self-center" />
+                <Col >
+                    <div class="media" style={{ marginLeft: '100px' }}>
+                        <img src={filmIndividual.Poster} height="500px" class="align-self-center" />
                     </div>
                 </Col>
                 <Col style={{ color: "white", backgroundColor: "grey" }}>
@@ -33,32 +38,41 @@ const BookingsPage = () => {
 
                         <FormGroup>
                             <Label for="filmSelect">Select Film</Label>
-                            <Input type="select" name="select" id="exampleSelect" onChange={({ target }) => {setTitle(target.value); 
-                           setShowingTime(films.find(item => item.title === `${title}`));}}>
+                            <Input
+                                type="select"
+                                name="select"
+                                id="filmSelect"
+                                onChange={({ target }) => {
+                                    setTitle(target.value);
+                                    setfilmIndividual(films.find(item => item.Title === title));
+                                    console.log(target.value);
+                                    console.log(filmIndividual);
+                                }}>
                                 {films.map((item) =>
-                                    <option obj = {item} value={item.Title}>{item.Title}</option>)}
+                                    <option obj={item} value={item.Title}>{item.Title}</option>)}
                             </Input>
                         </FormGroup>
                         {/* <FormGroup>
                             <Label for="showingTimes">Showing Times</Label>
-                            <Input type="select" name="select" id="exampleSelect" onChange={({ target }) => {setTitle(target.value); 
-                            setFilmObj(target.obj); }}>
-                                {films.map((item) =>
-                                    <option obj = {item} value={item.Showtimes}>{item.Showtimes}</option>)}
+                            <Input type="select" name="select" id="exampleSelect" onChange={({ target }) => { setShowingTime(target.value) }}>
+                                {filmIndividual.Showtimes.map((item) =>
+                                    <option value={item}>{item}</option>)}
+                                    {console.log(`showing times object: ${filmIndividual.Title}`)}
                             </Input>
 
                         </FormGroup> */}
                         <FormGroup>
                             <Label for="showingTimes">Showing Times</Label>
-                            {films.map((item) =>
+                            <br />
+                            {filmIndividual.Showtimes.map((item) =>
                                 <Badge color="warning" pill style={{ marginLeft: '15px' }}>
-                                    <Link to="" style={{ color: 'black' }}>{item.Showtimes}</Link>
+                                    <Link to="" style={{ color: 'black' }}>{item}</Link>
                                 </Badge>)}
 
                         </FormGroup>
                         <FormGroup>
                             <Label for="exampleEmail">Name</Label>
-                            <Input type="text" name="name" id="bookerName" placeholder="Name of Booker" onChange={({ target }) => setBookgerName(target.value)}/>
+                            <Input type="text" name="name" id="bookerName" placeholder="Name of Booker" onChange={({ target }) => setBookgerName(target.value)} />
                         </FormGroup>
                         <FormGroup>
                             <Label for="seating">Seating</Label>
@@ -96,10 +110,10 @@ const BookingsPage = () => {
                                 </Col>
                             </Row>
                             {/* <Input type="text" name="name" id="bookerName" placeholder="Name of Booker" /> */}
-                            
-                            
+
+
                         </FormGroup>
-                        <Button color="warning" style={{width: 100}}>Pay Now</Button>
+                        <Button color="warning" style={{ width: 100 }}>Pay Now</Button>
                     </Form>
                     {showingTime}
                 </Col>
