32763aa8281ebd212623bb70353af0658188f058
calfonsoQA <75623928+calfonsoQA@users.noreply.github.com>
2021-03-08 12:21:18 +0000

    finished bookings frontend and backend

--- frontend/src/Pages/BookingsAndPayment/BookingsPage.jsx
+++ frontend/src/Pages/BookingsAndPayment/BookingsPage.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import films from '../../Resources/films.json';
-import { Button, Form, FormGroup, Label, Input, FormText, Col, Row, Badge } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, FormText, Col, Row, Badge,  Modal, ModalHeader, ModalBody, ModalFooter, FormFeedback  } from 'reactstrap';
 import { Link, } from 'react-router-dom';
+import { DB_URL, BOOKINGS_URL } from "../../Resources/CONSTS.json";
+import axios from "axios";
 const BookingsPage = (booknow) => {
 
     const [poster, setPoster] = useState('');
@@ -16,6 +18,16 @@ const BookingsPage = (booknow) => {
     const [filmObj, setFilmObj] = useState(films);
     const [filmIndividual, setfilmIndividual] = useState(films.find(item => item.Title === title));
     
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
+
+    const dismissModal = () => {
+        toggle();
+        setTimeout(() => {
+            
+        }, 1000);
+    }
 
 
     // setShowingTime(films.find(films.Showtimes, films.Showtimes, `${title}`));
@@ -37,6 +49,22 @@ const BookingsPage = (booknow) => {
 
     }
 
+    const setPrice = () =>{
+        setTotalPrice(7.2*adult+6.5*child+6.5*concession);
+    }
+
+    const create = (e) => {
+        e.preventDefault();
+        axios.post(`${BOOKINGS_URL}/create`, { title, showingTime, bookerName, adult, child, concession, totalPrice })
+            .then((res) => {
+                console.log(res.data);
+                toggle();
+                
+            }).catch((err) => {
+                console.log(err);
+            })
+    }
+
 
     return (
         <div className="container" >
@@ -47,7 +75,7 @@ const BookingsPage = (booknow) => {
                     </div>
                 </Col>
                 <Col className="rounded" style={{ color: "white", backgroundColor: "grey" }}>
-                    <Form>
+                    <Form onSubmit={create}>
 
                         <FormGroup>
                             <Label for="filmSelect">Select Film</Label>
@@ -104,7 +132,9 @@ const BookingsPage = (booknow) => {
                             <Row>
                                 <Col>Adult: £7.20</Col>
                                 <Col>
-                                    <Input type="select" name="select" id="adultSelect" onChange={({ target }) => setAdult(target.value)}>
+                                    <Input type="select" name="select" id="adultSelect" onChange={({ target }) => {setAdult(target.value);
+                                    
+                                    }} onBlur = { setPrice}>
                                         <option>0</option>
                                         <option>1</option>
                                         <option>2</option>
@@ -116,7 +146,9 @@ const BookingsPage = (booknow) => {
                             <Row>
                                 <Col>Child: £6.50</Col>
                                 <Col>
-                                    <Input type="select" name="select" id="childSelect" onChange={({ target }) => setChild(target.value)}>
+                                    <Input type="select" name="select" id="childSelect" onChange={({ target }) => {setChild(target.value);
+                                    
+                                    }} onBlur = { setPrice}>
                                         <option>0</option>
                                         <option>1</option>
                                         <option>2</option>
@@ -128,7 +160,9 @@ const BookingsPage = (booknow) => {
                             <Row>
                                 <Col>Concession: £6.50</Col>
                                 <Col>
-                                    <Input type="select" name="select" id="concessionSelect" onChange={({ target }) => setConcession(target.value)}>
+                                    <Input type="select" name="select" id="concessionSelect" onChange={({ target }) => {setConcession(target.value);
+                                    
+                                    }} onBlur = { setPrice}>
                                         <option>0</option>
                                         <option>1</option>
                                         <option>2</option>
@@ -138,7 +172,8 @@ const BookingsPage = (booknow) => {
                                 </Col>
                             </Row>
                             <Row>
-                                <Col>Total: £{(7.2*adult+7.2*child+7.2*concession).toFixed(2)} </Col>
+                                {/* <Col>Total: £{(7.2*adult+6.5*child+6.5*concession).toFixed(2)}  </Col> */}
+                                <Col>Total: £{(totalPrice).toFixed(2)}  </Col>
                                 <Col>
                                     {/* {totalPriceCalc(adult,child,concession)} */}
                                     {/* £{(7.2*adult+7.2*child+7.2*concession).toFixed(2)} */}
@@ -151,10 +186,30 @@ const BookingsPage = (booknow) => {
                         {/* <Link to={{ pathname: `/bookings/${movie.Title}` }} style={{ color: 'white' }}>Book Now</Link> */}
                         {/* <Button color="warning" style={{ width: 100 }}>Pay Now</Button> */}
 
-                        <Button color="warning" style={{ width: 100 }}> <Link to={{ pathname: `/payment/` }} style={{ color: 'black' }}>Book Now</Link> </Button>
+                        {/* <Button type="submit" color="warning" style={{ width: 100 }}> <Link to={{ pathname: `/payment/` }} style={{ color: 'black' }}>Book Now</Link> </Button> */}
+                        {/* <Button onSubmit={create} color="warning" style={{ width: 100 }}> Confirm Booking</Button> */}
+                        <Button onClick={create} color="warning"> Confirm Booking</Button>
                     </Form>
                 </Col>
             </Row>
+
+            <Modal isOpen={modal} toggle={toggle}>
+                <ModalHeader toggle={toggle}>Please confirm</ModalHeader>
+                <ModalBody>
+                <p>Title: {title}</p>
+                <p> Showing Time: {showingTime}</p>
+                <p>  Name: {bookerName}</p>
+                <p>  Adult: {adult}</p>
+                <p>  Child: {child}</p>
+                <p> Concession: {concession}</p>
+                <p> Total Price: £{totalPrice.toFixed(2)}</p>
+                    
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                    <Button color="secondary" onClick={dismissModal}><Link to={{ pathname: `/payment/` }} style={{ color: 'black' }}>Pay Now</Link></Button>
+                </ModalFooter>
+            </Modal>
         </div>
     )
 
--- frontend/src/Pages/BookingsAndPayment/PaymentPage.jsx
+++ frontend/src/Pages/BookingsAndPayment/PaymentPage.jsx
@@ -7,7 +7,7 @@ import { Toast, ToastBody, ToastHeader, ToastFooter } from "reactstrap";
 // require('dotenv').config();
 
 // const stripePromise = loadStripe(process.env.PUBLISHABLE_KEY);
-const stripePromise = loadStripe('');
+const stripePromise = loadStripe('pk_test_51IRJCSK9eOXpEB8jQfIX7nJGm8jrshmJJ0mISdUCUAYbjT4lV9hZLxvCfclftnED2Q7rgLz7KVeUWEDR0vhfCawY00r9aDXSb8');
 
 // console.log(process.env.PUBLISHABLE_KEY)
 
